5933a985ed97c707cccf631086ad697f34e2a6e7
Trisout <81978700+trisout78@users.noreply.github.com>
2024-02-13 11:33:18 +0100

    Initial commit

---
+++ src/fr/trisout/betterwhitelist/Main.java
@@ -0,0 +1,22 @@
+package fr.trisout.betterwhitelist;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener {
+    @EventHandler
+    public void onEnable() {
+        this.getServer().getPluginManager().registerEvents(this, this);
+        saveDefaultConfig();
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        reloadConfig();
+            if (!getConfig().getStringList("whitelist").contains(event.getPlayer().getName())) {
+                event.getPlayer().kickPlayer("§cYou are not whitelisted on this server.");
+            }
+        }
+    }
