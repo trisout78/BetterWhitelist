0c642bc16f1b99b79b00ffcb5291386ff4daba86
Trisout <81978700+trisout78@users.noreply.github.com>
2024-04-28 13:23:52 +0200

    Added HostnameWhitelist feature

--- src/main/java/fr/trisout/betterwhitelist/Main.java
+++ src/main/java/fr/trisout/betterwhitelist/Main.java
@@ -1,16 +1,33 @@
 package fr.trisout.betterwhitelist;
 
+import fr.trisout.betterwhitelist.utils.Util;
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.Configuration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import java.util.Set;
+import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerLoginEvent.Result;
 
 public class Main extends JavaPlugin implements Listener {
+
+    private Set<String> validHostNames;
+    private String warning;
+    private boolean ignoreCase;
+    private boolean blockLegacy;
+
     @Override
     public void onEnable() {
         this.getServer().getPluginManager().registerEvents(this, this);
         saveDefaultConfig();
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvents(this, this);
+        reloadConfig();
+
     }
 
     @EventHandler
@@ -20,7 +37,28 @@ public class Main extends JavaPlugin implements Listener {
                 event.getPlayer().kickPlayer("§cYou are not whitelisted on this server.");
             }
         }
+    @EventHandler
+    public void onLogin(PlayerLoginEvent event) {
+        String hostname = event.getHostname();
+        int port = hostname.indexOf(":");
+        if (port != -1) {
+            hostname = hostname.substring(0, port);
+        }
+        if (isBlocked(hostname)) {
+            event.disallow(Result.KICK_OTHER, warning);
+        }
+    }
 
+    private boolean isBlocked(String host) {
+        if (host == null) {
+            return blockLegacy;
+        } else {
+            if (ignoreCase) {
+                host = host.toLowerCase();
+            }
+            return !validHostNames.contains(host);
+        }
+    }
 
     @EventHandler
     public void onPlayerJoin(PlayerQuitEvent event) {
@@ -28,6 +66,15 @@ public class Main extends JavaPlugin implements Listener {
         if (!getConfig().getStringList("whitelist").contains(event.getPlayer().getName())) {
             event.setQuitMessage(null);
         }
+    }
+    @Override
+    public void reloadConfig() {
+        super.reloadConfig();
+        Configuration config = getConfig();
+        warning = ChatColor.translateAlternateColorCodes('&', config.getString("warning"));
+        ignoreCase = config.getBoolean("ignore-case", true);
+        validHostNames = Util.getHostNames(config.getStringList("allowed-host-names"), ignoreCase);
+        blockLegacy = config.getBoolean("block-legacy", true);
     }
 }
 
