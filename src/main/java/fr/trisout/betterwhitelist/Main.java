144e40d61c0349c412f9cc1d12ba4c37fe9d931a
Trisout <81978700+trisout78@users.noreply.github.com>
2024-05-19 12:11:59 +0200

    Revert "Code Cleanup"
    This reverts commit 08f3eaa4e3f9ee384a2b7a94af50e228fd1345e4.

--- src/main/java/fr/trisout/betterwhitelist/Main.java
+++ src/main/java/fr/trisout/betterwhitelist/Main.java
@@ -1,31 +1,67 @@
 package fr.trisout.betterwhitelist;
 
+import fr.trisout.betterwhitelist.utils.Util;
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.Configuration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import java.util.Set;
+import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerLoginEvent.Result;
 
 public class Main extends JavaPlugin implements Listener {
 
-    private HostnameWhitelist hostnameWhitelist;
+    private Set<String> validHostNames;
+    private String warning;
+    private boolean ignoreCase;
+    private boolean blockLegacy;
+    private boolean HostnameEnabled;
 
     @Override
     public void onEnable() {
         this.getServer().getPluginManager().registerEvents(this, this);
         saveDefaultConfig();
         PluginManager pm = getServer().getPluginManager();
-        pm.registerEvents(hostnameWhitelist, this);
+        pm.registerEvents(this, this);
         reloadConfig();
+
     }
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-        hostnameWhitelist.reloadConfigHostname(getConfig());
+        reloadConfigHostname();
         reloadConfig();
-        if (!getConfig().getStringList("whitelist").contains(event.getPlayer().getName())) {
-            event.getPlayer().kickPlayer("§cYou are not whitelisted on this server.");
+            if (!getConfig().getStringList("whitelist").contains(event.getPlayer().getName())) {
+                event.getPlayer().kickPlayer("§cYou are not whitelisted on this server.");
+            }
+        }
+    @EventHandler
+    public void onLogin(PlayerLoginEvent event) {
+        if (!HostnameEnabled) {
+            return;
+        }
+        String hostname = event.getHostname();
+        int port = hostname.indexOf(":");
+        if (port != -1) {
+            hostname = hostname.substring(0, port);
+        }
+        if (isBlocked(hostname)) {
+            event.disallow(Result.KICK_OTHER, warning);
+        }
+    }
+
+    private boolean isBlocked(String host) {
+        if (host == null) {
+            return blockLegacy;
+        } else {
+            if (ignoreCase) {
+                host = host.toLowerCase();
+            }
+            return !validHostNames.contains(host);
         }
     }
 
@@ -36,9 +72,14 @@ public class Main extends JavaPlugin implements Listener {
             event.setQuitMessage(null);
         }
     }
-
-    public void reloadConfig() {
+    public void reloadConfigHostname() {
         super.reloadConfig();
-        hostnameWhitelist = new HostnameWhitelist(getConfig());
+        Configuration config = getConfig();
+        warning = ChatColor.translateAlternateColorCodes('&', config.getString("warning"));
+        ignoreCase = config.getBoolean("ignore-case", true);
+        validHostNames = Util.getHostNames(config.getStringList("allowed-host-names"), ignoreCase);
+        blockLegacy = config.getBoolean("block-legacy", true);
+        HostnameEnabled = config.getBoolean("hostname-enabled", true);
     }
 }
+
